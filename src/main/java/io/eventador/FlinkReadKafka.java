4abc0b62bab3d3ddc3322ef407d0aa0f7c5be6a5
jtadrosEventador <jtadros@eventador.io>
2020-09-15 07:18:16 -0500

    Updated for flink 1.10.1

--- src/main/java/io/eventador/FlinkReadKafka.java
+++ src/main/java/io/eventador/FlinkReadKafka.java
@@ -1,15 +1,23 @@
 package io.eventador;
 
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
-import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+
+
 
 public class FlinkReadKafka {
+
+    public static final String TOPIC_ARG = "topic";
+
+
     public static void main(String[] args) throws Exception {
         // Read parameters from command line
+
+
         final ParameterTool params = ParameterTool.fromArgs(args);
 
         if(params.getNumberOfParameters() < 3) {
@@ -17,16 +25,16 @@ public class FlinkReadKafka {
             return;
         }
 
+        String topic = params.get(FlinkReadKafka.TOPIC_ARG);
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
         env.getConfig().setRestartStrategy(RestartStrategies.fixedDelayRestart(4, 10000));
         env.enableCheckpointing(300000); // 300 seconds
         env.getConfig().setGlobalJobParameters(params);
 
-        DataStream<String> messageStream = env
-                .addSource(new FlinkKafkaConsumer010<>(
-                        params.getRequired("topic"),
-                        new SimpleStringSchema(),
-                        params.getProperties()));
+
+
+        DataStream<String> messageStream = env.addSource(new FlinkKafkaConsumer<>(topic, new SimpleStringSchema(),params.getProperties()));
 
         // Print Kafka messages to stdout - will be visible in logs
         messageStream.print();
@@ -34,3 +42,5 @@ public class FlinkReadKafka {
         env.execute("FlinkReadKafka");
     }
 }
+
+
